c8de8d9c27633d2d331e03b169b05fe5be6a892b
MarwanGhareeb <marwanghareeb146@gmail.com>
2026-04-26 14:03:18 +0300

    feat: implement ThemeExtension and BuildContext extensions

---
+++ lib/core/utils/extensions/context_extensions.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:tech_nest/core/theme/app_colors_extension.dart';
+
+extension ThemeContextExtensions on BuildContext {
+  ThemeData get theme => Theme.of(this);
+
+  TextTheme get textTheme => theme.textTheme;
+
+  ColorScheme get colorScheme => theme.colorScheme;
+
+  AppColorsExtension get colors =>
+      theme.extension<AppColorsExtension>() ??
+      (throw Exception('AppColorsExtension not found in theme'));
+}
